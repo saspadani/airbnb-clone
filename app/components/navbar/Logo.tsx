8e93020dd4056e6190f55ed617dfb570a8013e6d
saspadani <aspadani@gmail.com>
2023-04-07 22:53:13 +0700

    navbar

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -9,10 +9,11 @@ const Logo = () => {
   return (
     <Image
       alt='logo'
-      className='hidden md:block cursor-pointer'
+      className='w-auto hidden md:block cursor-pointer'
       height='100'
       width='100'
       src='/images/logo.png'
+      priority={true}
     />
   );
 };
